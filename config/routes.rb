7e78d9f71bb411e72c6171f7e7115ed904c552d6
Jordan Luft <jordanluft2000@gmail.com>
2018-09-05 08:30:32 -0500

    First Blog Attempt

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  resources :posts
+
+
+get 'home' => 'posts#index'
+
+post 'home' => 'posts#index'
+
+get 'newpost' => 'posts#new'
+
+post 'newpost' => 'posts#new'
+
+  root 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
